ce7ddbe9c329d262138159ac023052ab389429fe
Orvil D'silva <orvildsilva@gmail.com>
2021-07-28 20:51:01 +0530

    added blog parameter in blog_details / created blogs list

--- lib/app_screens/blog_details_pg.dart
+++ lib/app_screens/blog_details_pg.dart
@@ -1,14 +1,19 @@
 import 'package:amigoproject/app_screens/blog_comments.dart';
 import 'package:flutter/material.dart';
+import 'blog.dart';
 
 class BlogDetails extends StatefulWidget {
   // const BlogDetails({Key key}) : super(key: key);
-
+  Blog blog;
+  BlogDetails({ required this.blog});
   @override
-  _BlogDetailsState createState() => _BlogDetailsState();
+  _BlogDetailsState createState() => _BlogDetailsState(blog: blog);
 }
 
 class _BlogDetailsState extends State<BlogDetails> {
+
+  Blog blog;
+  _BlogDetailsState({required this.blog});
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
@@ -32,7 +37,7 @@ class _BlogDetailsState extends State<BlogDetails> {
                   child: Column(
                     children: [
                       Text(
-                        'The OCD Stories',
+                        blog.title,
                         style: TextStyle(
                             fontFamily: 'Poppins',
                             fontSize: 26.67,
@@ -44,7 +49,7 @@ class _BlogDetailsState extends State<BlogDetails> {
                       ),
                       Row(
                         children: [
-                          Text('Date\tTime\t-Author '),
+                          Text('Date\tTime\t-Author-${blog.author} '),
                           SizedBox(
                             width: 140,
                           ),
@@ -76,8 +81,7 @@ class _BlogDetailsState extends State<BlogDetails> {
                             fontWeight: FontWeight.w300,
                             // height: 27
                           ),
-                          text:
-                              "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.\nIt is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
+                          text: blog.text
                         ),
                       )
                     ],
